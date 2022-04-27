cf757f9d30a8b78347d14764672f42a6c74e9352
samkirsch10 <samkirsch@gmail.com>
2022-04-27 08:27:54 -0400

    Learned there is a json output which is WAY eaiser to parse

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -54,26 +52,36 @@ func metrics() {
 			var sleepTime time.Duration
 			sleepTime = 600
 
-			out := runCMD("cd /opt/lsi/perccli/ && ./perccli /c0/e32/sall show | tail -n +14")
-			diskMap := parseOutput(out)
+			out := runCMD("cd /opt/lsi/perccli/ && ./perccli /c0/e32/sall show J")
+			var diskMap ControllerResponse
+			if err := json.Unmarshal([]byte(out), &diskMap); err != nil {
+				log.Println("ERROR", err.Error())
+			}
+
+			// diskMap := parseOutput(out)
 			disksOnline.Set(0)
-			for _, disk := range diskMap {
-				if disk["status"] == "Onln" {
+			for _, disk := range diskMap.Controllers[0].ResponseData.DriveInformation {
+				if disk.State == "Onln" {
 					log.Println(disk)
 					disksOnline.Inc()
-				}
-				if disk["rebuild_percent"] != "-" {
-					// Allows next run to happen sooner to check next percentage
-					sleepTime = 5
-
-					perc, err := strconv.ParseFloat(disk["rebuild_percent"], 10)
-					if err != nil {
-						log.Println("Could not convert string rebuild_percent to float64!")
-						log.Println(err)
+				} else if disk.State == "Rbld" {
+					log.Println(disk)
+					cmd := fmt.Sprintf("cd /opt/lsi/perccli/ && ./perccli /c0/e32/s%d show rebuild J", disk.DID)
+					out = runCMD(cmd)
+					var rebuildInfo ControllerRebuildResponse
+					if err := json.Unmarshal([]byte(out), &rebuildInfo); err != nil {
+						log.Println("ERROR", err.Error())
 					} else {
-						diskRebuildPercent.WithLabelValues(disk["deviceID"]).Set(perc)
+						percent := rebuildInfo.Controllers[0].ResponseData[0].Progress
+						log.Println("Got rebuild percent for disk:", percent)
+						diskRebuildPercent.WithLabelValues(fmt.Sprintf("%d", disk.DID)).Set(float64(percent))
+						// Allows next run to happen sooner to check next percentage
+						sleepTime = 30
 					}
 
+				} else {
+					log.Println("Disk in unknown state!")
+					log.Println(disk)
 				}
 			}
 
@@ -83,38 +91,38 @@ func metrics() {
 	}()
 }
 
-func parseOutput(cmdOut string) []map[string]string {
-	var diskOutput []map[string]string
+// func parseOutput(cmdOut string) []map[string]string {
+// 	var diskOutput []map[string]string
 
-	var re = regexp.MustCompile(`32:(?P<slotID>[0-9]+)\s+(?P<deviceID>[0-9]+)\s(?P<status>[a-zA-Z]+)\s+(?P<driveGroup>[0-9]+)\s+(?P<size>[0-9.]+\s[GMT][B])\sSATA\s[SHD]{3}\s[YN]\s+[YN]\s+[0-9B]+\s(?P<model>[a-zA-Z0-9-\s]+\s[UD])(.*)`)
+// 	var re = regexp.MustCompile(`32:(?P<slotID>[0-9]+)\s+(?P<deviceID>[0-9]+)\s(?P<status>[a-zA-Z]+)\s+(?P<driveGroup>[0-9]+)\s+(?P<size>[0-9.]+\s[GMT][B])\sSATA\s[SHD]{3}\s[YN]\s+[YN]\s+[0-9B]+\s(?P<model>[a-zA-Z0-9-\s]+\s[UD])(.*)`)
 
-	for _, disk := range strings.Split(cmdOut, "\n") {
-		if strings.Contains(disk, "-----------------") {
-			break
-		}
-		match := re.FindStringSubmatch(disk)
-		if len(match) > 0 {
-			result := make(map[string]string)
-			for i, cgName := range re.SubexpNames() {
-				if i != 0 && cgName != "" {
-					if cgName == "model" && (strings.HasSuffix(match[i], "U") || strings.HasSuffix(match[i], "D")) {
-						result[cgName] = strings.TrimSpace(match[i][:len(match[i])-2])
-					} else {
-						result[cgName] = match[i]
-					}
-				}
-			}
-			if result["status"] != "Onln" && result["status"] != "Offln" {
-				cmd := fmt.Sprintf("cd /opt/lsi/perccli/ && ./perccli /c0/e32/s%s show rebuild | tail -n +11 | head -1 | awk '{print $2}'", result["deviceID"])
-				result["rebuild_percent"] = runCMD(cmd)
-			} else {
-				result["rebuild_percent"] = "-"
-			}
-			diskOutput = append(diskOutput, result)
-		}
-	}
-	return diskOutput
-}
+// 	for _, disk := range strings.Split(cmdOut, "\n") {
+// 		if strings.Contains(disk, "-----------------") {
+// 			break
+// 		}
+// 		match := re.FindStringSubmatch(disk)
+// 		if len(match) > 0 {
+// 			result := make(map[string]string)
+// 			for i, cgName := range re.SubexpNames() {
+// 				if i != 0 && cgName != "" {
+// 					if cgName == "model" && (strings.HasSuffix(match[i], "U") || strings.HasSuffix(match[i], "D")) {
+// 						result[cgName] = strings.TrimSpace(match[i][:len(match[i])-2])
+// 					} else {
+// 						result[cgName] = match[i]
+// 					}
+// 				}
+// 			}
+// 			if result["status"] != "Onln" && result["status"] != "Offln" {
+// 				cmd := fmt.Sprintf("cd /opt/lsi/perccli/ && ./perccli /c0/e32/s%s show rebuild | tail -n +11 | head -1 | awk '{print $2}'", result["deviceID"])
+// 				result["rebuild_percent"] = runCMD(cmd)
+// 			} else {
+// 				result["rebuild_percent"] = "-"
+// 			}
+// 			diskOutput = append(diskOutput, result)
+// 		}
+// 	}
+// 	return diskOutput
+// }
 
 func runCMD(cmd string) string {
 	var b bytes.Buffer
---
+++ vars.go
@@ -0,0 +1,48 @@
+package main
+
+type DriveInfo struct {
+	Slot  string `json:"EID:Slt"`
+	DID   int
+	State string
+	DG    int
+	Size  string
+	Intf  string
+	Med   string
+	SED   string
+	PI    string
+	SeSz  string
+	Model string
+	Sp    string
+	Type  string
+}
+
+type RebuildInfo struct {
+	ID       string `json:"Drive-ID"`
+	Progress int    `json:"Progress%"`
+	Status   string
+	ETA      string `json:"Estimated Time Left"`
+}
+
+type CmdStatus struct {
+	Version     string `json:"CLI Version"`
+	OS          string `json:"Operating system"`
+	Controller  int
+	Status      string
+	Description string
+}
+
+type ControllerResponse struct {
+	Controllers []struct {
+		CommandStatus CmdStatus `json:"Command Status"`
+		ResponseData  struct {
+			DriveInformation []DriveInfo `json:"Drive Information"`
+		} `json:"Response Data"`
+	}
+}
+
+type ControllerRebuildResponse struct {
+	Controllers []struct {
+		CommandStatus CmdStatus     `json:"Command Status"`
+		ResponseData  []RebuildInfo `json:"Response Data"`
+	}
+}
